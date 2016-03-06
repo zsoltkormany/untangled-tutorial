fb8994cf75a52d462a109f51c2495d64b057edb5
Tony Kay <tony@teamunify.com>
2016-03-05 22:15:04 -0800

    updated docs on client

--- src/tutorial/untangled_tutorial/F_Untangled_Client.cljs
+++ src/tutorial/untangled_tutorial/F_Untangled_Client.cljs
@@ -32,6 +32,7 @@
       - Complete handling of tempids, merging, attribute conflict resolution, and more!
       - Application locale handling
       - Support VCR viewer recording with internal mutations that can submit support requests
+      - Support for refreshing the entire UI on hot code reload (with your help)
   - It mounts the application UI on the given DOM element (you can pass a real node or string ID)
 
   Some additional things that are availabe (which we'll cover soon):
@@ -51,10 +52,71 @@
 
   The primary components of this layout are:
 
-  - An entry point for production that does nothing *but* mount the app
-  - A core namespace that creates the application and loads things like i18n support
-  - A development namespace file that mounts the app in development mode
   - A project file that is configured for running both the server, client, and tests with hot code reload.
+  - An entry point for production that does nothing *but* mount the app. Nothing but the project build refers to this,
+  so we don't accidentally try to do entry point operations more than once. It can also be used to do things
+  like disable console logging.
+  - A development-only namespace file that mounts the app in development mode. Only the dev build includes this source.
+  - A core namespace that creates the application and loads things like i18n support. This is referred to by the production
+  build and the development-only namespace.
+
+  ## Enabling re-render on hot code reload
+
+  Om and React do their best to prevent unnecessary rendering. During development we'd like to
+  force a refresh that whenever code reloads so we can see our changes.
+  In order to do this:
+
+  - Untangled forces a full UI refresh is mount is called on an already mounted app. If you specify that the
+  development namespace (which always mounts the app) is reloaded every time, then that is sufficient.
+  - The application itself has a (protocol) method named `refresh`. You can configure
+  figwheel to invoke a function that calls thta after each load.
+
+  However, in order to get *React* to actually re-render, you have to cooperate by adding a react
+  key from your app state onto your top-level DOM node, like this:
+
+  ```
+  (defui Root
+     static om/IQuery
+     (query [this] [:ui/react-key ...])
+     Object
+     (render [this]
+        (let [{:keys [ui/react-key ...]} (om/props this)]
+          (dom/div #js { :key react-key } ...))))
+  ```
+
+  The Untangled refresh will automatically set this key in your app state to a new unique value
+  every time, ensuring that the app state has changed (which will cause Om to allow a re-render) and
+  the top-level key change will cause React to force a full DOM diff (ignoring the fact that the
+  rest of the recursive state has not changed).
+
+  ## Using the REPL
+
+  ### Making sure you're connected to the right browser/tab
+
+  If you're running more than one build in Figwheel, the REPL will only be connected to one browser tab
+  at a time. You can run `(fig-status)` to see what builds are running and how many browsers are
+  connected to each.
+
+  If you're trying to look at app state, make sure ONLY one browser is connected to it, otherwise
+  you'll confuse yourself!
+
+  To ensure that you're talking to the tab of the correct build, you should do the following at
+  the REPL:
+
+  ```
+  cljs.user=> *:cljs/quit*
+  Choose focus build for CLJS REPL (tutorial, client, test) or quit > client
+  Launching ClojureScript REPL for build: client
+  ```
+
+  ### Viewing application state
+
+  The sample project `user` namespace includes a helper function called `log-app-state`. By default
+  it will show the entire application state; however, this is often too much (if it gets too big, your
+  REPL will give an error). Instead, you can supply it with a keyword and it will look that up in the
+  app state and show just that bit instead.
+
+
   "
   )
 
